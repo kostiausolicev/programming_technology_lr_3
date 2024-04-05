5ed83db5fbe52a4b1220cefd8fda30c9ca148f34
vlade <Connection-vlademyanov@yandex.ru>
2024-04-06 00:52:09 +0300

    Wrote base calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,182 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CalculatorApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+// Основной виджет приложения Flutter.
+class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Счёты)', // Заголовок приложения.
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue, // Основной цвет приложения.
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CalculatorScreen(), // Основной экран калькулятора.
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+// Состояние виджета калькулятора.
+class CalculatorScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CalculatorScreenState createState() => _CalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _output = '0'; // Текущий вывод калькулятора.
+  String _outputHistory = ''; // История операций.
+  double _num1 = 0; // Первое число для операции.
+  double _num2 = 0; // Второе число для операции.
+  String _operand = ''; // Текущая операция.
 
-  void _incrementCounter() {
+  // Обработчик нажатия на кнопку.
+  void buttonPressed(String buttonText) {
+    if (buttonText == 'C') {
+      // Если нажата кнопка "C", очистить все данные.
+      _output = '0';
+      _outputHistory = '';
+      _num1 = 0;
+      _num2 = 0;
+      _operand = '';
+    } else if (buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/') {
+      // Если нажата кнопка операции, сохранить первое число и операцию.
+      _num1 = double.parse(_output);
+      _operand = buttonText;
+      _output = '0';
+      _outputHistory += '$_num1 $_operand';
+    } else if (buttonText == '.') {
+      // Если нажата кнопка десятичной точки, добавить ее к числу, если ее еще нет.
+      if (!_output.contains('.')) {
+        _output += buttonText;
+      }
+    } else if (buttonText == '=') {
+      // Если нажата кнопка "=", выполнить операцию и показать результат.
+      _num2 = double.parse(_output);
+      _outputHistory += '$_num2 = ';
+      switch (_operand) {
+        case '+':
+          _output = (_num1 + _num2).toString();
+          break;
+        case '-':
+          _output = (_num1 - _num2).toString();
+          break;
+        case '*':
+          _output = (_num1 * _num2).toString();
+          break;
+        case '/':
+          _output = (_num1 / _num2).toString();
+          break;
+      }
+      _num1 = 0;
+      _num2 = 0;
+      _operand = '';
+    } else {
+      // Если нажата обычная кнопка с цифрой, добавить ее к текущему выводу.
+      if (_output == '0') {
+        _output = buttonText;
+      } else {
+        _output += buttonText;
+      }
+    }
+
+    // Обновить состояние виджета для обновления интерфейса.
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _output;
+      _outputHistory;
     });
+  }
+
+  // Создание кнопки для калькулятора.
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: ElevatedButton(
+          onPressed: () => buttonPressed(buttonText),
+          child: Text(
+            buttonText,
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Счёты)'), // Заголовок на экране калькулятора.
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          // Отображение истории операций.
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              _outputHistory,
+              style: TextStyle(fontSize: 20.0),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          // Отображение текущего вывода.
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              _output,
+              style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          // Размещение кнопок для цифр и операций.
+          Column(
+            children: [
+              Row(
+                children: [
+                  buildButton('7'),
+                  buildButton('8'),
+                  buildButton('9'),
+                  buildButton('/'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('4'),
+                  buildButton('5'),
+                  buildButton('6'),
+                  buildButton('*'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('1'),
+                  buildButton('2'),
+                  buildButton('3'),
+                  buildButton('-'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('0'),
+                  buildButton('.'),
+                  buildButton('C'),
+                  buildButton('+'),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton('='),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
